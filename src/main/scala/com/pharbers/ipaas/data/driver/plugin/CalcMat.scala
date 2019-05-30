135401bb4ba2715e696352c7ad16cb1d2f7e31b9
Cui <cui_cjm@163.com>
2019-05-30 17:55:18 +0800

    dcs 的插件单元测试

--- src/main/scala/com/pharbers/ipaas/data/driver/plugin/CalcMat.scala
+++ src/main/scala/com/pharbers/ipaas/data/driver/plugin/CalcMat.scala
@@ -5,12 +5,12 @@ import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.IntegerType
 
-class CalcMat [T <: Map[String, PhWorkArgs[_]]](args: PhWorkArgs[T]) extends WindowPluginArgs(args) with PhOperatorTrait{
+case class CalcMat [T <: Map[String, PhWorkArgs[_]]](args: PhWorkArgs[T]) extends WindowPluginArgs(args) with PhOperatorTrait{
     override val name: String = "calc mat"
     override val defaultArgs: PhWorkArgs[_] = PhNoneArgs
 
     override def perform(pr: PhWorkArgs[_]): PhWorkArgs[_] = {
-        val windowYearOnYear = Window.partitionBy(partitionColumnNames.map(x => col(x)): _*).orderBy(col(dateColName).cast(IntegerType)).rangeBetween(-100, 0)
+        val windowYearOnYear = Window.partitionBy(partitionColumnNames.map(x => col(x)): _*).orderBy(col(dateColName).cast(IntegerType)).rangeBetween(-99, 0)
 
         PhColArgs(sum(col(valueColumnName)).over(windowYearOnYear))
     }
--- src/main/scala/com/pharbers/ipaas/data/driver/plugin/addByWhen.scala
+++ src/main/scala/com/pharbers/ipaas/data/driver/plugin/addByWhen.scala
@@ -1,12 +1,25 @@
 package com.pharbers.ipaas.data.driver.plugin
 
-import com.pharbers.ipaas.data.driver.api.work.{PhColArgs, PhNoneArgs, PhOperatorTrait, PhWorkArgs}
+import com.pharbers.ipaas.data.driver.api.work.{PhColArgs, PhMapArgs, PhNoneArgs, PhOperatorTrait, PhWorkArgs}
 import org.apache.spark.sql.functions._
 
-class addByWhen[T <: Map[String, PhWorkArgs[_]]](args: PhWorkArgs[T]) extends PhOperatorTrait{
+/** Adds a key/value pair to this map, returning a new map.
+  *  @param    kv the key/value pair
+  *  @tparam   B1 the type of the value in the key/value pair.
+  *  @return   a new map with the new binding added to this map
+  *
+  *  @example  def + (kv: (A, B)): Map[A, B]
+  */
+case class addByWhen[T <: Map[String, PhWorkArgs[_]]](args: PhWorkArgs[T]) extends PhOperatorTrait{
     override val name: String = "add column by when"
     override val defaultArgs: PhWorkArgs[_] = PhNoneArgs
 
+	/** Adds a key/value pair to this map, returning a new map.
+	  *  @param    pr the key/value pair
+	  *  @return   a new map with the new binding added to this map
+	  *
+	  *  @example  def + (kv: (A, B)): Map[A, B]
+	  */
     override def perform(pr: PhWorkArgs[_]): PhWorkArgs[_] = {
         val condition = args.get.getOrElse("condition",throw new Exception("not found condition")).get.asInstanceOf[String]
         val trueValue = args.get.getOrElse("trueValue",throw new Exception("not found trueValue")).get
