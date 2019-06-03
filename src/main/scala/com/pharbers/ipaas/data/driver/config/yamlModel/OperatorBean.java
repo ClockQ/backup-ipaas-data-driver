ce846cb27eb23299238b7712f87432ed3de70291
dcs <419787760@qq.com>
2019-06-03 15:09:41 +0800

    读取json或者yaml的job配置文件

--- src/main/scala/com/pharbers/ipaas/data/driver/config/yamlModel/OperatorBean.java
+++ src/main/scala/com/pharbers/ipaas/data/driver/config/yamlModel/OperatorBean.java
@@ -1,4 +1,52 @@
 package com.pharbers.ipaas.data.driver.config.yamlModel;
 
+import java.util.List;
+import java.util.Map;
+
 public class OperatorBean {
+    private String name;
+    private String factory;
+    private String oper;
+    private Map<String, String> args;
+    private List<PluginBean> plugins;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFactory() {
+        return factory;
+    }
+
+    public void setFactory(String factory) {
+        this.factory = factory;
+    }
+
+    public String getOper() {
+        return oper;
+    }
+
+    public void setOper(String oper) {
+        this.oper = oper;
+    }
+
+    public Map<String, String> getArgs() {
+        return args;
+    }
+
+    public void setArgs(Map<String, String> args) {
+        this.args = args;
+    }
+
+    public List<PluginBean> getPlugins() {
+        return plugins;
+    }
+
+    public void setPlugins(List<PluginBean> plugins) {
+        this.plugins = plugins;
+    }
 }
--- src/main/scala/com/pharbers/ipaas/data/driver/config/yamlModel/PluginBean.java
+++ src/main/scala/com/pharbers/ipaas/data/driver/config/yamlModel/PluginBean.java
@@ -1,4 +1,22 @@
 package com.pharbers.ipaas.data.driver.config.yamlModel;
 
 public class PluginBean {
+    private String name;
+    private String factory;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFactory() {
+        return factory;
+    }
+
+    public void setFactory(String factory) {
+        this.factory = factory;
+    }
 }
