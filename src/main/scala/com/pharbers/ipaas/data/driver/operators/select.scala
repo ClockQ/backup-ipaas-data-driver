c93006bb89c27e7041c8f9c721c4d0ec25d5a23c
clock <2567729201@qq.com>
2019-06-04 20:21:46 +0800

    添加select 和 distinctByKey

--- src/main/scala/com/pharbers/ipaas/data/driver/operators/select.scala
+++ src/main/scala/com/pharbers/ipaas/data/driver/operators/select.scala
@@ -7,16 +7,15 @@ import com.pharbers.ipaas.data.driver.api.work._
   * @author: clock
   * @date: 2019-05-28 17:21
   */
-case class addColumn(plugin: PhPluginTrait, name: String, defaultArgs: PhWorkArgs[_]) extends PhOperatorTrait {
+case class select(plugin: PhPluginTrait, name: String, defaultArgs: PhWorkArgs[_]) extends PhOperatorTrait {
 
     override def perform(pr: PhWorkArgs[_]): PhWorkArgs[_] = {
         val defaultMapArgs = defaultArgs.toMapArgs[PhWorkArgs[_]]
         val prMapArgs = pr.toMapArgs[PhWorkArgs[_]]
         val inDFName = defaultMapArgs.getAs[PhStringArgs]("inDFName").get.get
-        val newColName = defaultMapArgs.getAs[PhStringArgs]("newColName").get.get
+        val selectLst = defaultMapArgs.getAs[PhStringArgs]("selects").get.get.split("#")
         val inDF = prMapArgs.getAs[PhDFArgs](inDFName).get.get
-        val func = plugin.perform(pr).toColArgs.get
-        val outDF = inDF.withColumn(newColName, func)
+        val outDF = inDF.select(selectLst.head, selectLst.tail: _*)
 
         PhDFArgs(outDF)
     }
