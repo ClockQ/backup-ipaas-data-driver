559631c1a93695584ee025149833c5a1cb685d2c
Cui <cui_cjm@163.com>
2019-06-05 17:11:36 +0800

    max yaml 待测试

--- src/main/scala/com/pharbers/ipaas/data/driver/plugin/AddByWhen.scala
+++ src/main/scala/com/pharbers/ipaas/data/driver/plugin/AddByWhen.scala
@@ -6,9 +6,7 @@ import org.apache.spark.sql.functions._
 /**when插件
   * @author dcs
   */
-case class AddByWhen() extends PhPluginTrait{
-    override val name: String = "add column by when"
-    override val defaultArgs: PhWorkArgs[_] = PhNoneArgs
+case class AddByWhen(name: String, defaultArgs: PhWorkArgs[_]) extends PhPluginTrait{
 
 	/**
 	  *   @param   pr  实际类型PhMapArgs.condition -> when表达式，trueValue -> 满足条件的赋值，otherValue -> 不满足条件的赋值
@@ -19,7 +17,7 @@ case class AddByWhen() extends PhPluginTrait{
 	  *   @history 记录修改历史，暂时在这儿写最后一个修改的是谁
 	  */
     override def perform(pr: PhWorkArgs[_]): PhWorkArgs[_] = {
-        val args = pr.toMapArgs[PhWorkArgs[_]]
+        val args = defaultArgs.toMapArgs[PhWorkArgs[_]]
         val condition = args.get.getOrElse("condition",throw new Exception("not found condition")).get.asInstanceOf[String]
         val trueValue = args.get.getOrElse("trueValue",throw new Exception("not found trueValue")).get
         val otherValue = args.get.getOrElse("otherValue",throw new Exception("not found otherValue")).get
