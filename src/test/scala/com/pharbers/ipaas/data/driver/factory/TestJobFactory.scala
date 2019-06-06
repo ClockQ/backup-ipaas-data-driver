af37ce006eb5451740574b566ccb182c947b6593
clock <2567729201@qq.com>
2019-06-06 14:20:42 +0800

    max 单元测试（未通过）

--- src/test/scala/com/pharbers/ipaas/data/driver/factory/TestJobFactory.scala
+++ src/test/scala/com/pharbers/ipaas/data/driver/factory/TestJobFactory.scala
@@ -15,26 +15,24 @@ import org.scalatest.FunSuite
   * @note
   */
 class TestJobFactory extends FunSuite{
-    test("get job from job factory"){
-        val jobs = Config.readJobConfig("pharbers_config/max.yaml")
-        val phJobs = jobs.map(x => {
-            PhFactory.getMethodMirror(x.getFactory)(x).asInstanceOf[PhJobFactory].inst()
-        })
+    test("test nhwa max"){
         implicit val sd: PhSparkDriver = PhSparkDriver("testSparkDriver")
-        val dfCheck = sd.setUtil(readParquet()).readParquet("hdfs:///test/qi/qi/max_true")
-//        val dfCheck = sd.setUtil(readCsv()).readCsv("hdfs:///data/nhwa/pha_config_repository1809/Nhwa_201809_Offline_MaxResult_20181126.csv")
-        val df = phJobs.head.perform(PhMapArgs(Map.empty))
-//        df.toMapArgs[PhDFArgs].get.foreach(x => {
-//            println(x._1)
-//            x._2.get.show(false)
-//        })
-        df.toMapArgs[PhDFArgs].get("maxResultDF").get.show(false)
-        val maxDF = df.toMapArgs[PhDFArgs].get("maxResultDF").get
-        println(dfCheck.agg(sum("f_units")).first.get(0))
-        println(dfCheck.agg(sum("f_sales")).first.get(0))
+
+        val jobs = Config.readJobConfig("pharbers_config/max.yaml")
+        val phJobs = jobs.map(x => PhFactory.getMethodMirror(x.getFactory)(x).asInstanceOf[PhJobFactory].inst())
+        val result = phJobs.head.perform(PhMapArgs(Map.empty))
+
+        val maxDF = result.toMapArgs[PhDFArgs].get("maxResultDF").get
+        val maxTrueDF = sd.setUtil(readParquet()).readParquet("hdfs:///test/qi/qi/new_max_true")
+
+        println(maxDF.count())
+        println(maxTrueDF.count())
 
         println(maxDF.agg(sum("f_units")).first.get(0))
         println(maxDF.agg(sum("f_sales")).first.get(0))
+
+        println(maxTrueDF.agg(sum("f_units")).first.get(0))
+        println(maxTrueDF.agg(sum("f_sales")).first.get(0))
     }
 
     test("test nhwa clean"){
