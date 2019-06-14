8f72a30dc82ead43ac7be38e0a9871b887a8dd7a
clock <2567729201@qq.com>
2019-06-14 20:31:26 +0800

    修改Operation Exception 注释

--- src/main/scala/com/pharbers/ipaas/data/driver/exceptions/PhOperatorException.scala
+++ src/main/scala/com/pharbers/ipaas/data/driver/exceptions/PhOperatorException.scala
@@ -1,37 +1,30 @@
 package com.pharbers.ipaas.data.driver.exceptions
 
-/** 功能描述
-  *捕获job运行异常及job->action->operator链
+/** 捕获job运行异常及job->action->operator链
+  *
   * @author dcs
-  * @version 0.0
+  * @version 0.1
   * @since 2019/6/11 15:27
   * @note
   */
-case class PhOperatorException(names: Seq[String], exception: Exception) extends Exception{
-    /** 功能描述
+case class PhOperatorException(names: Seq[String], exception: Exception) extends Exception {
+    /** 获取异常StackTrace
       *
-     获取异常StackTrace
       * @return _root_.scala.Array[_root_.java.lang.StackTraceElement]
-      * @author EDZ
-      * @version 0.0
+      * @author dcs
+      * @version 0.1
       * @since 2019/6/11 15:54
-      * @note 一些值得注意的地方
-      * @example {{{这是一个例子}}}
       */
-    override def getStackTrace: Array[StackTraceElement] = {
+    override def getStackTrace: Array[StackTraceElement] =
         exception.getStackTrace
-    }
-/** 功能描述
-  *
- 获取异常描述
-  * @return _root_.scala.Predef.String
-  * @author EDZ
-  * @version 0.0
-  * @since 2019/6/11 15:55
-  * @note 一些值得注意的地方
-  * @example {{{这是一个例子}}}
-  */
-    override def getMessage: String = {
+
+    /** 获取异常描述
+      *
+      * @return _root_.scala.Predef.String
+      * @author dcs
+      * @version 0.1
+      * @since 2019/6/11 15:55
+      */
+    override def getMessage: String =
         (names ++ exception.getStackTrace.map(x => x.toString)).mkString("\n")
-    }
 }
