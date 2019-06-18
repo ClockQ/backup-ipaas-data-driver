afc6dbd69d75a39ae085eba5a073172039af06a2
clock <2567729201@qq.com>
2019-06-18 19:57:44 +0800

    nhwa panel 完成

--- src/test/scala/com/pharbers/ipaas/data/driver/run/TestNhwaMax.scala
+++ src/test/scala/com/pharbers/ipaas/data/driver/run/TestNhwaMax.scala
@@ -4,9 +4,9 @@ import env.configObj._
 import org.scalatest.FunSuite
 import org.apache.spark.sql.functions._
 import com.pharbers.ipaas.data.driver.api.work._
-import com.pharbers.ipaas.data.driver.libs.log.{PhLogDriver, formatMsg}
 import com.pharbers.ipaas.data.driver.libs.spark.PhSparkDriver
-import com.pharbers.ipaas.data.driver.libs.spark.util.readParquet
+import com.pharbers.ipaas.data.driver.libs.log.{PhLogDriver, formatMsg}
+import com.pharbers.ipaas.data.driver.libs.spark.util.{readCsv, readParquet}
 
 class TestNhwaMax extends FunSuite {
     implicit val sd: PhSparkDriver = PhSparkDriver("test-driver")
@@ -43,39 +43,39 @@ class TestNhwaMax extends FunSuite {
             "logDriver" -> PhLogDriverArgs(PhLogDriver(formatMsg("test_user", "test_traceID", "test_jobID")))
         )))
 
-        val panelDF = result.toMapArgs[PhDFArgs].get("reducedCpa").get
-//		val panelTrueDF = sd.setUtil(readParquet()).readParquet("hdfs:///test/qi/qi/1809_panel.csv")
+        val panelDF = result.toMapArgs[PhDFArgs].get("panelResult").get
+		val panelTrueDF = sd.setUtil(readCsv()).readCsv("hdfs:///test/qi/qi/1809_panel.csv")
 
         panelDF.show(false)
-//		panelTrueDF.show(false)
+		panelTrueDF.show(false)
 
         println(panelDF.count())
-//		println(panelTrueDF.count())
+		println(panelTrueDF.count())
 
         println(panelDF.agg(sum("UNITS")).first.get(0))
-//		println(panelTrueDF.agg(sum("Units")).first.get(0))
+		println(panelTrueDF.agg(sum("Units")).first.get(0))
 
         println(panelDF.agg(sum("SALES")).first.get(0))
-//		println(panelTrueDF.agg(sum("Sales")).first.get(0))
+		println(panelTrueDF.agg(sum("Sales")).first.get(0))
     }
 
-//	test("test nhwa max") {
-//		implicit val sd: PhSparkDriver = PhSparkDriver("testSparkDriver")
-//
-//		val jobs = configObj.readJobConfig("pharbers_config/max.yaml")
-//		val phJobs = jobs.map(x => getMethodMirror(x.getFactory)(x).asInstanceOf[PhJobFactory].inst())
-//		val result = phJobs.head.perform(PhMapArgs(Map.empty))
-//
-//		val maxDF = result.toMapArgs[PhDFArgs].get("maxResultDF").get
-//		val maxTrueDF = sd.setUtil(readParquet()).readParquet("hdfs:///test/qi/qi/new_max_true")
-//
-//		println(maxDF.count())
-//		println(maxTrueDF.count())
-//
-//		println(maxDF.agg(sum("f_units")).first.get(0))
-//		println(maxDF.agg(sum("f_sales")).first.get(0))
-//
-//		println(maxTrueDF.agg(sum("f_units")).first.get(0))
-//		println(maxTrueDF.agg(sum("f_sales")).first.get(0))
-//	}
+	test("test nhwa max") {
+        val phJobs = inst(readJobConfig("max_config/common/max.yaml"))
+        val result = phJobs.head.perform(PhMapArgs(Map(
+            "sparkDriver" -> PhSparkDriverArgs(sd),
+            "logDriver" -> PhLogDriverArgs(PhLogDriver(formatMsg("test_user", "test_traceID", "test_jobID")))
+        )))
+
+		val maxDF = result.toMapArgs[PhDFArgs].get("maxResultDF").get
+		val maxTrueDF = sd.setUtil(readParquet()).readParquet("hdfs:///test/qi/qi/new_max_true")
+
+		println(maxDF.count())
+		println(maxTrueDF.count())
+
+		println(maxDF.agg(sum("f_units")).first.get(0))
+        println(maxTrueDF.agg(sum("f_units")).first.get(0))
+
+        println(maxDF.agg(sum("f_sales")).first.get(0))
+        println(maxTrueDF.agg(sum("f_sales")).first.get(0))
+    }
 }
