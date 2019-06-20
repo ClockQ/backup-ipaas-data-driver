ba311d6cfce6ab0f500aa3d6472739c78cc8cdc2
clock <2567729201@qq.com>
2019-06-20 11:42:55 +0800

    提取单独测试

--- src/test/scala/com/pharbers/ipaas/data/driver/run/TestTqMax.scala
+++ src/test/scala/com/pharbers/ipaas/data/driver/run/TestTqMax.scala
@@ -20,7 +20,7 @@ package com.pharbers.ipaas.data.driver.run
 import com.pharbers.ipaas.data.driver.api.work._
 import com.pharbers.ipaas.data.driver.libs.log.{PhLogDriver, formatMsg}
 import com.pharbers.ipaas.data.driver.libs.spark.PhSparkDriver
-import com.pharbers.ipaas.data.driver.libs.spark.util.readCsv
+import com.pharbers.ipaas.data.driver.libs.spark.util.{readCsv, save2Parquet}
 import env.configObj.{inst, readJobConfig}
 import org.apache.spark.sql.functions._
 import org.scalatest.FunSuite
@@ -29,6 +29,76 @@ class TestTqMax extends FunSuite {
 	implicit val sd: PhSparkDriver = PhSparkDriver("test-driver")
 	sd.addJar("target/ipaas-data-driver-0.1.jar")
 	sd.sc.setLogLevel("ERROR")
+
+	test("clean tq RP panel") {
+		val phJobs = inst(readJobConfig("max_config/tq/RPcleanPanel.yaml"))
+		val result = phJobs.head.perform(PhMapArgs(Map(
+			"sparkDriver" -> PhSparkDriverArgs(sd),
+			"logDriver" -> PhLogDriverArgs(PhLogDriver(formatMsg("test_user", "test_traceID", "test_jobID")))
+		)))
+
+		val panelERD = result.toMapArgs[PhDFArgs].get("panelERD").get
+		val panelDF = sd.setUtil(readCsv()).readCsv("hdfs:///data/TQ/RP_Panel 201806.csv")
+
+		panelERD.show(false)
+		panelDF.show(false)
+
+		val panelERDCount = panelERD.count()
+		val panelDFCount = panelDF.count()
+		println(panelERDCount)
+		println(panelDFCount)
+		assert(panelERDCount == panelDFCount)
+
+		val panelERDUnits = panelERD.agg(sum("UNITS")).first.get(0).toString.toDouble
+		val panelDFUnits = panelDF.agg(sum("Units")).first.get(0).toString.toDouble
+		println(panelERDUnits)
+		println(panelDFUnits)
+		assert(Math.abs(panelERDUnits - panelDFUnits) < (panelDFUnits * 0.01))
+
+		val panelERDSales = panelERD.agg(sum("SALES")).first.get(0).toString.toDouble
+		val panelDFSales = panelDF.agg(sum("Sales")).first.get(0).toString.toDouble
+		println(panelERDSales)
+		println(panelDFSales)
+		assert(Math.abs(panelERDSales - panelDFSales) < (panelDFSales * 0.01))
+
+//		sd.setUtil(save2Parquet()).save2Parquet(panelERD, "hdfs:///workData/Panel/tq/rp/201806")
+	}
+
+	test("clean tq RP universe") {
+		val phJobs = inst(readJobConfig("max_config/tq/RPcleanUniverse.yaml"))
+		val result = phJobs.head.perform(PhMapArgs(Map(
+			"sparkDriver" -> PhSparkDriverArgs(sd),
+			"logDriver" -> PhLogDriverArgs(PhLogDriver(formatMsg("test_user", "test_traceID", "test_jobID")))
+		)))
+
+		val universeERD = result.toMapArgs[PhDFArgs].get("universeERD").get
+		val universeDF = sd.setUtil(readCsv()).readCsv("hdfs:///data/TQ/TQ_Universe_RP_20181126.csv")
+
+		universeERD.filter(!col("HOSPITAL_ID").startsWith("other")).show(false)
+
+		universeERD.show(false)
+		universeDF.show(false)
+
+		val universeERDCount = universeERD.count()
+		val universeDFCount = universeDF.count()
+		println(universeERDCount)
+		println(universeDFCount)
+//		assert(universeERDCount == universeDFCount) // 可能不相等，因为新旧 PHA_ID 问题
+
+		val universeERDFACTOR = universeERD.agg(sum("FACTOR")).first.get(0).toString.toDouble
+		val universeDFFACTOR = universeDF.agg(sum("FACTOR")).first.get(0).toString.toDouble
+		println(universeERDFACTOR)
+		println(universeDFFACTOR)
+//		assert(Math.abs(universeERDFACTOR - universeDFFACTOR) < (universeDFFACTOR * 0.01))
+
+		val universeERDWEST_MEDICINE_INCOME = universeERD.agg(sum("WEST_MEDICINE_INCOME")).first.get(0).toString.toDouble
+		val universeDFWEST_MEDICINE_INCOME = universeDF.agg(sum("WEST_MEDICINE_INCOME")).first.get(0).toString.toDouble
+		println(universeERDWEST_MEDICINE_INCOME)
+		println(universeDFWEST_MEDICINE_INCOME)
+//		assert(Math.abs(universeERDWEST_MEDICINE_INCOME - universeDFWEST_MEDICINE_INCOME) < (universeDFWEST_MEDICINE_INCOME * 0.01))
+
+//		sd.setUtil(save2Parquet()).save2Parquet(universeERD, "hdfs:///repository/universe_hosp/tq/rp")
+	}
 
 	test("test tq RP max") { //TODO 未通过
 		val phJobs = inst(readJobConfig("max_config/tq/RPmax.yaml"))
@@ -57,6 +127,76 @@ class TestTqMax extends FunSuite {
 		println(maxDFSales)
 		println(offlineSales)
 		assert(Math.abs(maxDFSales - offlineSales) < offlineSales * 0.01)
+	}
+
+	test("clean tp SA panel") {
+		val phJobs = inst(readJobConfig("max_config/tq/SAcleanPanel.yaml"))
+		val result = phJobs.head.perform(PhMapArgs(Map(
+			"sparkDriver" -> PhSparkDriverArgs(sd),
+			"logDriver" -> PhLogDriverArgs(PhLogDriver(formatMsg("test_user", "test_traceID", "test_jobID")))
+		)))
+
+		val panelERD = result.toMapArgs[PhDFArgs].get("panelERD").get
+		val panelDF = sd.setUtil(readCsv()).readCsv("hdfs:///data/TQ/SA_Panel 201806.csv")
+
+		panelERD.show(false)
+		panelDF.show(false)
+
+		val panelERDCount = panelERD.count()
+		val panelDFCount = panelDF.count()
+		println(panelERDCount)
+		println(panelDFCount)
+		assert(panelERDCount == panelDFCount)
+
+		val panelERDUnits = panelERD.agg(sum("UNITS")).first.get(0).toString.toDouble
+		val panelDFUnits = panelDF.agg(sum("Units")).first.get(0).toString.toDouble
+		println(panelERDUnits)
+		println(panelDFUnits)
+		assert(Math.abs(panelERDUnits - panelDFUnits) < (panelDFUnits * 0.01))
+
+		val panelERDSales = panelERD.agg(sum("SALES")).first.get(0).toString.toDouble
+		val panelDFSales = panelDF.agg(sum("Sales")).first.get(0).toString.toDouble
+		println(panelERDSales)
+		println(panelDFSales)
+		assert(Math.abs(panelERDSales - panelDFSales) < (panelDFSales * 0.01))
+
+//		sd.setUtil(save2Parquet()).save2Parquet(panelERD, "hdfs:///workData/Panel/tq/sa/201806")
+	}
+
+	test("clean tq SA universe") {
+		val phJobs = inst(readJobConfig("max_config/tq/SAcleanUniverse.yaml"))
+		val result = phJobs.head.perform(PhMapArgs(Map(
+			"sparkDriver" -> PhSparkDriverArgs(sd),
+			"logDriver" -> PhLogDriverArgs(PhLogDriver(formatMsg("test_user", "test_traceID", "test_jobID")))
+		)))
+
+		val universeERD = result.toMapArgs[PhDFArgs].get("universeERD").get
+		val universeDF = sd.setUtil(readCsv()).readCsv("hdfs:///data/TQ/TQ_Universe_SA_20181126.csv")
+
+		universeERD.filter(!col("HOSPITAL_ID").startsWith("other")).show(false)
+
+		universeERD.show(false)
+		universeDF.show(false)
+
+		val universeERDCount = universeERD.count()
+		val universeDFCount = universeDF.count()
+		println(universeERDCount)
+		println(universeDFCount)
+//		assert(universeERDCount == universeDFCount) // 可能不相等，因为新旧 PHA_ID 问题
+
+		val universeERDFACTOR = universeERD.agg(sum("FACTOR")).first.get(0).toString.toDouble
+		val universeDFFACTOR = universeDF.agg(sum("FACTOR")).first.get(0).toString.toDouble
+		println(universeERDFACTOR)
+		println(universeDFFACTOR)
+//		assert(Math.abs(universeERDFACTOR - universeDFFACTOR) < (universeDFFACTOR * 0.01))
+
+		val universeERDWEST_MEDICINE_INCOME = universeERD.agg(sum("WEST_MEDICINE_INCOME")).first.get(0).toString.toDouble
+		val universeDFWEST_MEDICINE_INCOME = universeDF.agg(sum("WEST_MEDICINE_INCOME")).first.get(0).toString.toDouble
+		println(universeERDWEST_MEDICINE_INCOME)
+		println(universeDFWEST_MEDICINE_INCOME)
+//		assert(Math.abs(universeERDWEST_MEDICINE_INCOME - universeDFWEST_MEDICINE_INCOME) < (universeDFWEST_MEDICINE_INCOME * 0.01))
+
+//		sd.setUtil(save2Parquet()).save2Parquet(universeERD, "hdfs:///repository/universe_hosp/tq/sa")
 	}
 
 	test("test tq SA max") {  //TODO 未通过
