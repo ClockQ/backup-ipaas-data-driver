5bca1c2bafabc2f1a7fa3f98de47bb484ca09e51
dcs <419787760@qq.com>
2019-09-09 10:19:33 +0800

    max submit

---
+++ src/main/scala/com/pharbers/ipaas/data/driver/api/job/PhProgressJob.scala
@@ -0,0 +1,14 @@
+package com.pharbers.ipaas.data.driver.api.job
+
+/** 功能描述
+  *
+  * @param args 构造参数
+  * @tparam T 构造泛型参数
+  * @author dcs
+  * @version 0.0
+  * @since 2019/09/05 19:45
+  * @note 一些值得注意的地方
+  */
+class PhProgressJob {
+
+}
---
+++ src/main/scala/com/pharbers/ipaas/data/driver/plugins/LitPlugin.scala
@@ -0,0 +1,14 @@
+package com.pharbers.ipaas.data.driver.plugins
+
+/** 功能描述
+  *
+  * @param args 构造参数
+  * @tparam T 构造泛型参数
+  * @author dcs
+  * @version 0.0
+  * @since 2019/09/06 18:28
+  * @note 一些值得注意的地方
+  */
+class LitPlugin {
+
+}
